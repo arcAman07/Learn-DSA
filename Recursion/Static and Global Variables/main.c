0e223df12ff171ed03681528fda68eaa6dc8cae1
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2021-11-08 16:13:18 +0530

    Add files via upload

---
+++ Recursion/Static and Global Variables/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int fun(int n)
+{
+  static int x = 0;
+  if (n>0)
+  {
+    x++;
+    return fun(n-1)+x;
+  }
+  else
+  {
+    return 0;
+  }
+
+}
+
+void main()
+{
+  int a = 5;
+  printf("%d ",fun(a));
+}
