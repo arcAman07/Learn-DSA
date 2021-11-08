dbadc5d67fdba33a8300e95a67c0764bb927ea22
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2021-11-08 17:24:04 +0530

    Add files via upload

---
+++ Recursion/Indirect Recursion/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void funA(int n)
+{
+  if (n>0)
+  {
+    printf("%d ",n);
+    funB(n-1);
+  }
+}
+
+void funB(int n)
+{
+  if (n>1)
+  {
+    printf("%d ",n);
+    funA(n/2);
+  }
+
+}
+
+int main()
+{
+  int a = 20;
+  funA(a);
+  return 0;
+}
