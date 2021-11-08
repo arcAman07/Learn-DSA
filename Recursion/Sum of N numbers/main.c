3dcd504a3396e0667499fcc51832e661fa6d0c52
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2021-11-08 08:42:37 +0530

    Add files via upload

---
+++ Recursion/Sum of N numbers/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int sum(int n)
+{
+  if (n==0){
+    return 0;
+  }
+  else {
+    return sum(n-1)+n;
+  }
+}
+
+int main()
+{
+  int a = sum(8);
+  printf("%d ",a);
+  return 0;
+}
