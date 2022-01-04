3759a394f8712194af547e1c813b8910af03c049
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2022-01-04 19:52:28 +0530

    Add files via upload

---
+++ Arrays Representations/Declaring an Array/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int A[5];
+  int B[5]={1,2,3,4,5};
+  int C[10]={2,4,6};
+  int D[5]={0};
+  int E[]={1,2,3,4,5,6};
+  printf("%d ",E[0]);
+  int i=0;
+  for (i=0;i<5;i++)
+  {
+
+  }
+  return 0;
+}
